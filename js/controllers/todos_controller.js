874e44bf1dc7dbe6391bde00bb72b9af08ee4c69
Rahlus Karl Morsi <github@metroplexweb.com>
2014-06-11 21:20:04 -0500

    Indicating When All Todos Are Complete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -25,6 +25,10 @@ Todos.TodosController = Ember.ArrayController.extend({
 		}
 	},
 
+	allAreDone: function(key, value){
+		return !!this.get('length') && this.isEvery('isCompleted');
+	}.property('@each.isCompleted'),
+
 	hasCompleted: function(){
 		return this.get('completed') > 0;
 	}.property('completed'),
